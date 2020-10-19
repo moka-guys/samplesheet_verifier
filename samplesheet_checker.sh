51cf16002ff6531c0974d27df1e487e5badac17a
Graeme Smith <graeme_c_smith@hotmail.com>
2020-10-19 18:47:03 +0100

    Fixed bug in parsing file name

--- samplesheet_checker.sh
+++ samplesheet_checker.sh
@@ -37,22 +37,29 @@ warning_message_array=${1}
 
 
 inotifywait -m $directory_to_watch -e create -e moved_to |
-    while read -r file; do
+    while IFS= read -r file; do
         echo "Change detected:" # Useful when debugging to know that script is running correctly
+        filepath=$(echo "$file" | awk -F " " '{print $1}')
+        file_name=$(echo "$file" | awk -F " " '{print $NF}')
+        absolute_file_path="$filepath$file_name"
+        echo "$absolute_file_path"
 
         ## Check the saved sample sheet is named correctly so that metadata can be extracted by the pipeline
     	# bash's limited regex will not cope with the regex I created to match correctly formatted filenames: ^[0-9]{6}_(.*)_[0-9]{4}_(.*)_(?>.*?)SampleSheet\.csv$
     	# To overcome this we can split the filename based on the _ delimiter:
-    	IFS='_' read -r -a array <<< "$file"
+    	IFS='_' read -r -a array <<< "$file_name"
+        echo "Hello"
+        printf '%s\n' "${array[@]}"
+        echo "Hello"
     	# We then reiterate over the array checking that each component of the file name matches the expected formatting and that the correct number of fields are present:
         if [[ ${array[0]} =~ [0-9]{6} && \
         	${array[2]} =~ [0-9]{4} && \
             ${array[4]} =~ "SampleSheet.csv" && \
             ${#array[@]} == 5 ]]; # Does the file match expected pattern
         then
-            echo "$file matches pattern"
+            echo "$file_name matches pattern"
         else
-            raise_warning "Sample sheet has been saved to workstation with the file name $file, which does not match the expected naming convention"
+            raise_warning "Sample sheet has been saved to workstation with the file name $file_name, which does not match the expected naming convention"
         fi
 
         ## Check that the contents of the SampleSheet pass some minimum criteria
@@ -62,7 +69,7 @@ inotifywait -m $directory_to_watch -e create -e moved_to |
         lines_detected_in_file=$(wc -l "$file")
         if [[ ! $lines_detected_in_file > $lines_expected_in_file ]];
         then
-        	raise_warning "$file contains only $lines_detected_in_file lines, but expected at least $lines_expected_in_file for valid file"
+        	raise_warning "$file_name contains only $lines_detected_in_file lines, but expected at least $lines_expected_in_file for valid file"
         fi
 
         # Check the header headings are correct
@@ -98,7 +105,8 @@ inotifywait -m $directory_to_watch -e create -e moved_to |
         fi
 
         # Check that the row headings are correct:
-        row_names=$("$file" | head -n 19 | tail -n 1)
+        row_names=$("$file" head -n 19 | tail -n 1)
+        echo row_names
         if [[ $row_names == "Sample_ID,Sample_Name,Sample_Plate,Sample_Well,I7_Index_ID,index,I5_Index_ID,index2,Sample_Project,Description" ]];
         then
             echo "Spreadsheet row names are correct"
